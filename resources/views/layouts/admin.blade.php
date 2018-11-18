eaa333d65a61a3e45f02c58ed035dd61d0934afa
Wathsara <wvd.51461@gmail.com>
2018-11-18 12:27:39 +0530

    Fixeded the dashboard css and view

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -50,12 +50,12 @@
             Tip 2: you can also add an image using data-image tag
         -->
 
-        <div class="logo"><a href="http://www.creative-tim.com/" class="simple-text logo-mini">
+        <div class="logo"><a href="#" class="simple-text logo-mini">
                 CT
             </a>
 
-            <a href="http://www.creative-tim.com/" class="simple-text logo-normal">
-                Creative Tim
+            <a href="#" class="simple-text logo-normal">
+                Chaturanga <br>AutoCare
             </a></div>
 
         <div class="sidebar-wrapper">
@@ -67,7 +67,7 @@
                 <div class="user-info">
                     <a data-toggle="collapse" href="#collapseExample" class="username">
                     <span>
-                       Tania Andrew
+                       {{Auth::user()->name}}
                       <b class="caret"></b>
                     </span>
                     </a>
@@ -117,204 +117,6 @@
                 </li>
 
 
-                <li class="nav-item ">
-                    <a class="nav-link" data-toggle="collapse" href="">
-                        <i class="material-icons">apps</i>
-                        <p> Components
-                            <b class="caret"></b>
-                        </p>
-                    </a>
-
-                    <div class="collapse" id="componentsExamples">
-                        <ul class="nav">
-                            <li class="nav-item ">
-                                <a class="nav-link" data-toggle="collapse" href="#componentsCollapse">
-                                    <span class="sidebar-mini"> MLT </span>
-                                    <span class="sidebar-normal"> Multi Level Collapse
-                              <b class="caret"></b>
-                            </span>
-
-                                </a>
-
-                                <div class="collapse" id="componentsCollapse">
-                                    <ul class="nav">
-                                        <li class="nav-item ">
-                                            <a class="nav-link" href="#0">
-                                                <span class="sidebar-mini"> E </span>
-                                                <span class="sidebar-normal"> Example </span>
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </li>
-                            <li class="nav-item ">
-                                <a class="nav-link" href="components/buttons.html">
-                                    <span class="sidebar-mini"> B </span>
-                                    <span class="sidebar-normal"> Buttons </span>
-                                </a>
-                            </li>
-                            <li class="nav-item ">
-                                <a class="nav-link" href="components/grid.html">
-                                    <span class="sidebar-mini"> GS </span>
-                                    <span class="sidebar-normal"> Grid System </span>
-                                </a>
-                            </li>
-                            <li class="nav-item ">
-                                <a class="nav-link" href="components/panels.html">
-                                    <span class="sidebar-mini"> P </span>
-                                    <span class="sidebar-normal"> Panels </span>
-                                </a>
-                            </li>
-                            <li class="nav-item ">
-                                <a class="nav-link" href="components/sweet-alert.html">
-                                    <span class="sidebar-mini"> SA </span>
-                                    <span class="sidebar-normal"> Sweet Alert </span>
-                                </a>
-                            </li>
-                            <li class="nav-item ">
-                                <a class="nav-link" href="components/notifications.html">
-                                    <span class="sidebar-mini"> N </span>
-                                    <span class="sidebar-normal"> Notifications </span>
-                                </a>
-                            </li>
-                            <li class="nav-item ">
-                                <a class="nav-link" href="components/icons.html">
-                                    <span class="sidebar-mini"> I </span>
-                                    <span class="sidebar-normal"> Icons </span>
-                                </a>
-                            </li>
-                            <li class="nav-item ">
-                                <a class="nav-link" href="components/typography.html">
-                                    <span class="sidebar-mini"> T </span>
-                                    <span class="sidebar-normal"> Typography </span>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-
-                <li class="nav-item ">
-                    <a class="nav-link" data-toggle="collapse" href="#formsExamples">
-                        <i class="material-icons">content_paste</i>
-                        <p> Forms
-                            <b class="caret"></b>
-                        </p>
-                    </a>
-
-                    <div class="collapse" id="formsExamples">
-                        <ul class="nav">
-                            <li class="nav-item ">
-                                <a class="nav-link" href="forms/regular.html">
-                                    <span class="sidebar-mini"> RF </span>
-                                    <span class="sidebar-normal"> Regular Forms </span>
-                                </a>
-                            </li>
-                            <li class="nav-item ">
-                                <a class="nav-link" href="forms/extended.html">
-                                    <span class="sidebar-mini"> EF </span>
-                                    <span class="sidebar-normal"> Extended Forms </span>
-                                </a>
-                            </li>
-                            <li class="nav-item ">
-                                <a class="nav-link" href="forms/validation.html">
-                                    <span class="sidebar-mini"> VF </span>
-                                    <span class="sidebar-normal"> Validation Forms </span>
-                                </a>
-                            </li>
-                            <li class="nav-item ">
-                                <a class="nav-link" href="forms/wizard.html">
-                                    <span class="sidebar-mini"> W </span>
-                                    <span class="sidebar-normal"> Wizard </span>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-
-                <li class="nav-item ">
-                    <a class="nav-link" data-toggle="collapse" href="#tablesExamples">
-                        <i class="material-icons">grid_on</i>
-                        <p> Tables
-                            <b class="caret"></b>
-                        </p>
-                    </a>
-
-                    <div class="collapse" id="tablesExamples">
-                        <ul class="nav">
-                            <li class="nav-item ">
-                                <a class="nav-link" href="tables/regular.html">
-                                    <span class="sidebar-mini"> RT </span>
-                                    <span class="sidebar-normal"> Regular Tables </span>
-                                </a>
-                            </li>
-                            <li class="nav-item ">
-                                <a class="nav-link" href="tables/extended.html">
-                                    <span class="sidebar-mini"> ET </span>
-                                    <span class="sidebar-normal"> Extended Tables </span>
-                                </a>
-                            </li>
-                            <li class="nav-item ">
-                                <a class="nav-link" href="tables/datatables.net.html">
-                                    <span class="sidebar-mini"> DT </span>
-                                    <span class="sidebar-normal"> DataTables.net </span>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-
-                <li class="nav-item ">
-                    <a class="nav-link" data-toggle="collapse" href="#mapsExamples">
-                        <i class="material-icons">place</i>
-                        <p> Maps
-                            <b class="caret"></b>
-                        </p>
-                    </a>
-
-                    <div class="collapse" id="mapsExamples">
-                        <ul class="nav">
-                            <li class="nav-item ">
-                                <a class="nav-link" href="maps/google.html">
-                                    <span class="sidebar-mini"> GM </span>
-                                    <span class="sidebar-normal"> Google Maps </span>
-                                </a>
-                            </li>
-                            <li class="nav-item ">
-                                <a class="nav-link" href="maps/fullscreen.html">
-                                    <span class="sidebar-mini"> FSM </span>
-                                    <span class="sidebar-normal"> Full Screen Map </span>
-                                </a>
-                            </li>
-                            <li class="nav-item ">
-                                <a class="nav-link" href="maps/vector.html">
-                                    <span class="sidebar-mini"> VM </span>
-                                    <span class="sidebar-normal"> Vector Map </span>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-
-                <li class="nav-item ">
-                    <a class="nav-link" href="widgets.html">
-                        <i class="material-icons">widgets</i>
-                        <p> Widgets </p>
-                    </a>
-                </li>
-
-                <li class="nav-item ">
-                    <a class="nav-link" href="charts.html">
-                        <i class="material-icons">timeline</i>
-                        <p> Charts </p>
-                    </a>
-                </li>
-
-                <li class="nav-item ">
-                    <a class="nav-link" href="calendar.html">
-                        <i class="material-icons">date_range</i>
-                        <p> Calendar </p>
-                    </a>
-                </li>
 
             </ul>
 
@@ -339,7 +141,7 @@
                     </div>
 
 
-                    <a class="navbar-brand" href="#pablo">Dashboard</a>
+                    <a class="navbar-brand" href="#pablo">Admin</a>
                 </div>
 
                 <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation" data-target="#navigation-example">
@@ -349,60 +151,6 @@
                     <span class="navbar-toggler-icon icon-bar"></span>
                 </button>
 
-                <div class="collapse navbar-collapse justify-content-end">
-
-
-                    <form class="navbar-form">
-                        <div class="input-group no-border">
-                            <input type="text" value="" class="form-control" placeholder="Search...">
-                            <button type="submit" class="btn btn-white btn-round btn-just-icon">
-                                <i class="material-icons">search</i>
-                                <div class="ripple-container"></div>
-                            </button>
-                        </div>
-                    </form>
-
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#pablo">
-                                <i class="material-icons">dashboard</i>
-                                <p class="d-lg-none d-md-block">
-                                    Stats
-                                </p>
-                            </a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link" href="http://example.com/" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="material-icons">notifications</i>
-                                <span class="notification">5</span>
-                                <p class="d-lg-none d-md-block">
-                                    Some Actions
-                                </p>
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-                                <a class="dropdown-item" href="#">Mike John responded to your email</a>
-                                <a class="dropdown-item" href="#">You have 5 new tasks</a>
-                                <a class="dropdown-item" href="#">You're now friend with Andrew</a>
-                                <a class="dropdown-item" href="#">Another Notification</a>
-                                <a class="dropdown-item" href="#">Another One</a>
-                            </div>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="#pablo">
-                                <i class="material-icons">person</i>
-                                <p class="d-lg-none d-md-block">
-                                    Account
-                                </p>
-                            </a>
-                        </li>
-                    </ul>
-
-
-
-
-
-                </div>
             </div>
         </nav>
 
